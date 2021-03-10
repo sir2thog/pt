43e01a9708737141b176ac2a86837ee8c054c81b
sir2thog <80360868+sir2thog@users.noreply.github.com>
2021-03-10 14:39:33 -0600

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,15 @@
+enum ActionKind {
+    Walking,
+    Idle,
+    Jumping
+}
 tiles.setTilemap(tilemap`level1 start`)
 let p1Name = game.askForString("")
 let p1 = sprites.create(assets.image`forestMonkey0`, SpriteKind.Player)
+tiles.placeOnTile(p1, tiles.getTileLocation(7, 8))
 music.setVolume(20)
 forever(function () {
+    controller.moveSprite(p1)
+    scene.cameraFollowSprite(p1)
     music.playMelody("F G A D E F E D ", 260)
 })
