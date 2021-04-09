991f1e5d7d65861dca3115165906bd67f94566dc
sir2thog <80360868+sir2thog@users.noreply.github.com>
2021-04-09 15:17:28 -0500

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -3,9 +3,17 @@ enum ActionKind {
     Idle,
     Jumping
 }
+function heal (potion0: boolean) {
+    if (info.life() < 3) {
+        while (true) {
+            info.setLife(info.life() + 1)
+        }
+    }
+}
 let p1: Sprite = null
 scene.centerCameraAt(7, 8)
 tiles.setTilemap(tilemap`level1 start`)
+let potion = 1
 let p1Name = game.askForString("")
 tiles.placeOnTile(p1, tiles.getTileLocation(7, 8))
 p1 = sprites.create(assets.image`skellyFront`, SpriteKind.Player)
